a896e1e86bd07bc03bacddce3800b817e09cd37c
dawhey <dawhey@gmail.com>
2015-11-11 21:56:27 +0100

    Added calendar_layout file with listview to implement

--- app/src/main/java/pl/edu/zut/mad/appwizut2/fragments/CaldroidCustomFragment.java
+++ app/src/main/java/pl/edu/zut/mad/appwizut2/fragments/CaldroidCustomFragment.java
@@ -3,8 +3,12 @@ package pl.edu.zut.mad.appwizut2.fragments;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.roomorama.caldroid.CaldroidFragment;
@@ -19,6 +23,7 @@ import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
 import pl.edu.zut.mad.appwizut2.R;
+import pl.edu.zut.mad.appwizut2.activities.MainActivity;
 import pl.edu.zut.mad.appwizut2.models.DayParity;
 import pl.edu.zut.mad.appwizut2.utils.WeekParityChecker;
 
@@ -26,9 +31,11 @@ public class CaldroidCustomFragment extends CaldroidFragment {
 
     private final WeekParityChecker checker = new WeekParityChecker();
     private static final SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy.MM.dd");
+    private static final SimpleDateFormat REVERSED_FORMATTER = new SimpleDateFormat("dd.MM.yyyy");
     private final ArrayList<DayParity> parityList;
     private static Bundle bundle;
     private boolean enableExecuteRefresh = true;
+    private TextView clickedDate;
 
     public CaldroidCustomFragment() throws ExecutionException, InterruptedException {
 
@@ -36,10 +43,42 @@ public class CaldroidCustomFragment extends CaldroidFragment {
 
     }
 
-    public static CaldroidCustomFragment newInstance() throws ExecutionException, InterruptedException {
-        CaldroidCustomFragment fragment = new CaldroidCustomFragment();
-        fragment.setArguments(bundle);
-        return fragment;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // michalbednarski: Hacky workaround for Caldroid's saved state mishandling
+        if (savedInstanceState != null) {
+            // Delete info for child fragment manager
+            savedInstanceState.remove("android:support:fragments");
+        }
+        // Call super
+        super.onCreate(savedInstanceState);
+        // TODO: We completely disabled Caldroid's state saving (since it's only causing problems)
+        // Now we have to implement retaining currently selected month/year outselves
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: remove initUI method
+        // (It shouldn't belong to onCreateView, this forces you to know day parity synchronously)
+        initUI();
+
+        // Get calendar view from superclass
+        ViewGroup calendarView = (ViewGroup) super.onCreateView(inflater, container, savedInstanceState);
+
+        // Disable state saving on superclass'es view
+        // This is workaround for Caldroid's improper handling of state saving
+        for (int i = 0; i < calendarView.getChildCount(); i++) {
+            calendarView.getChildAt(i).setSaveFromParentEnabled(false);
+        }
+
+        //setting toolbar title
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Kalendarz");
+
+        // Wrap calendarView into out fragment
+        ViewGroup wrapper = (ViewGroup) inflater.inflate(R.layout.calendar_layout, container, false);
+        clickedDate = (TextView) wrapper.findViewById(R.id.dateTextView);
+        wrapper.addView(calendarView, 0);
+        return wrapper;
     }
 
     private void initUI(){
@@ -89,8 +128,7 @@ public class CaldroidCustomFragment extends CaldroidFragment {
     public CaldroidListener listener = new CaldroidListener() {
         @Override
         public void onSelectDate(Date date, View view) {
-            Toast.makeText(getActivity().getApplicationContext(), FORMATTER.format(date),
-                    Toast.LENGTH_SHORT).show();
+            clickedDate.setText("Wydarzenia " + REVERSED_FORMATTER.format(date));
         }
 
         @Override
