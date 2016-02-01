ed2a1295812f83321ceafdb24eb2d9fe50d0642c
michalbednarski <bednarski.michal2@gmail.com>
2016-02-01 14:30:13 +0100

    Add completion callback and shuffling

--- app/src/main/java/pl/edu/zut/mad/appwizut2/fragments/AboutUsFragment.java
+++ app/src/main/java/pl/edu/zut/mad/appwizut2/fragments/AboutUsFragment.java
@@ -1,22 +1,21 @@
 package pl.edu.zut.mad.appwizut2.fragments;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import pl.edu.zut.mad.appwizut2.R;
+import pl.edu.zut.mad.appwizut2.views.PuzzleImageView;
 
 /**
  * Created by dawid on 02.12.15.
  */
-public class AboutUsFragment extends Fragment {
+public class AboutUsFragment extends Fragment implements PuzzleImageView.OnSolvedListener {
 
     @Nullable
     @Override
@@ -25,6 +24,14 @@ public class AboutUsFragment extends Fragment {
         View v = inflater.inflate(R.layout.about_us_layout, container, false);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.nav_about_us);
 
+        ((PuzzleImageView) v.findViewById(R.id.mad_team_photo)).setOnSolvedListener(this);
+
         return v;
+    }
+
+    @Override
+    public void onPuzzleSolved(int difficulty) {
+        // TODO
+        Log.v("AboutUsFragment", "Solved at difficulty " + difficulty);
     }
 }
